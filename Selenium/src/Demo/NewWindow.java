a79ed7f33a26533342e35553321ef29bba55fb54
Sravyaitis <165852988+Sravyaitis@users.noreply.github.com>
2024-11-25 06:36:23 -0800

    Add files via upload

---
+++ Selenium/src/Demo/NewWindow.java
@@ -0,0 +1,50 @@
+package Demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.WebDriver.TargetLocator;
+import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NewWindow {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		
+		//switching window
+		//driver.switchTo().newWindow(WindowType.TAB);
+		//driver.switchTo().newWindow(WindowType.WINDOW);
+		
+		Set<String> handles=driver.getWindowHandles();
+		Iterator<String> it = handles.iterator();
+		String parentWindowId=it.next();
+		String childWindowId=it.next();
+		
+		driver.switchTo().window(childWindowId);
+		driver.get("https://rahulshettyacademy.com/");
+		String courseName=driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
+		
+		driver.switchTo().window(parentWindowId);
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		File file = name.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("logo.png"));
+		
+		//get height and width 
+		System.out.println(name.getRect().getDimension().getHeight());
+		System.out.println(name.getRect().getDimension().getWidth());
+		
+	}
+
+}
---
+++ Selenium/src/Demo/RelativeLoc.java
@@ -0,0 +1,33 @@
+package Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
+public class RelativeLoc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver  = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		
+		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+
+		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+		
+		WebElement dobEditBox=driver.findElement(By.cssSelector("[for='dateofBirth']"));
+		driver.findElement(with(By.tagName("input")).below(dobEditBox)).click();
+		
+		WebElement iceCream = driver.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+		driver.findElement(with(By.tagName("input")).toLeftOf(iceCream)).click();
+		
+		WebElement radioButton = driver.findElement(By.id("inlineRadio1"));
+		System.out.println(driver.findElement(with(By.tagName("label")).toRightOf(radioButton)).getText());
+		
+	
+	}
+
+}
